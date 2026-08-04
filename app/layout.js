53de628579adb80ffca828411ff4bab8dd9d93ec
godozicodes <ozichineke003@gmail.com>
2026-08-04 10:29:41 +0100

    Add protected site layout

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'For Joy ❤️',
+  description: 'A private love website for Joy and Ozioma',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
